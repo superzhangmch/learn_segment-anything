3d3993a1349cdc0434d118ec0e0eba7d4033829c
Zhang Miaochang <superzhangmch@163.com>
2023-08-19 21:34:32 +0800

    Update mask_decoder.py

--- segment_anything/modeling/mask_decoder.py
+++ segment_anything/modeling/mask_decoder.py
@@ -47,7 +47,15 @@ class MaskDecoder(nn.Module):
         self.num_multimask_outputs = num_multimask_outputs
 
         self.iou_token = nn.Embedding(1, transformer_dim)
-        self.num_mask_tokens = num_multimask_outputs + 1
+        self.num_mask_tokens = num_multimask_outputs + 1 
+        ''' 为什么要多一个mask：原文17页，附录A：为了降低多prompt时不必要的训练成本
+                Ambiguity is much rarer with multiple prompts and the three output masks will usually become similar. 
+                To minimize computation of degenerate losses at training and ensure the single unambiguous mask receives
+                a regular gradient signal, we only predict a single mask when more than one prompt is given.
+                This is accomplished by adding a fourth output token for an additional mask prediction. This
+                fourth mask is never returned for a single prompt and is the only mask returned for multiple prompts.
+        '''
+        
         self.mask_tokens = nn.Embedding(self.num_mask_tokens, transformer_dim)
 
         self.output_upscaling = nn.Sequential(
@@ -59,7 +67,7 @@ class MaskDecoder(nn.Module):
         )
         self.output_hypernetworks_mlps = nn.ModuleList(
             [
-                MLP(transformer_dim, transformer_dim, transformer_dim // 8, 3)
+                MLP(transformer_dim, transformer_dim, transformer_dim // 8, 3) # 参数: in_dim, hidden_dim, out_dim, layers_cnt
                 for i in range(self.num_mask_tokens)
             ]
         )
@@ -72,8 +80,8 @@ class MaskDecoder(nn.Module):
         self,
         image_embeddings: torch.Tensor,
         image_pe: torch.Tensor,
-        sparse_prompt_embeddings: torch.Tensor,
-        dense_prompt_embeddings: torch.Tensor,
+        sparse_prompt_embeddings: torch.Tensor, # for prompt: 点和box的编码
+        dense_prompt_embeddings: torch.Tensor,  # for prompt: mask 的编码
         multimask_output: bool,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
@@ -97,12 +105,15 @@ class MaskDecoder(nn.Module):
             sparse_prompt_embeddings=sparse_prompt_embeddings,
             dense_prompt_embeddings=dense_prompt_embeddings,
         )
-
+        # masks.shape == [bs, num_mask_tokens, h, w]
+        
         # Select the correct mask or masks for output
         if multimask_output:
             mask_slice = slice(1, None)
         else:
             mask_slice = slice(0, 1)
+        关于上面mask_slice. 原文17页，附录A：为了降低多prompt时不必要的训练成本, 故分情况训练。预测时，可以直接拿到预测结果，也可以拿到三个力度预测结果
+        
         masks = masks[:, mask_slice, :, :]
         iou_pred = iou_pred[:, mask_slice]
 
@@ -113,38 +124,42 @@ class MaskDecoder(nn.Module):
         self,
         image_embeddings: torch.Tensor,
         image_pe: torch.Tensor,
-        sparse_prompt_embeddings: torch.Tensor,
-        dense_prompt_embeddings: torch.Tensor,
+        sparse_prompt_embeddings: torch.Tensor,  # points prompt
+        dense_prompt_embeddings: torch.Tensor,   # mask prompt
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Predicts masks. See 'forward' for more details."""
         # Concatenate output tokens
-        output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight], dim=0)
-        output_tokens = output_tokens.unsqueeze(0).expand(sparse_prompt_embeddings.size(0), -1, -1)
-        tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
+        output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight], dim=0) 
+        # iou_token.shape = [1, emb_dim], mask_token.shape = [3+1, emb_dim], 3+1: mask cnt
+        # 额外增加几个特殊token，并下一步拼到point tokens上
+        
+        output_tokens = output_tokens.unsqueeze(0).expand(sparse_prompt_embeddings.size(0), -1, -1) # 扩展到batch_size个
+        tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)  # sparse_prompt_embeddings.shape = [bs, point_cnt, emb_dim]
 
         # Expand per-image data in batch direction to be per-mask
-        src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
-        src = src + dense_prompt_embeddings
+        src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0) # 单个img的img_emb扩展到bs个
+        src = src + dense_prompt_embeddings # dense_emb.shape = [bs, emb_dim==c, h=ori_img_h/16, w]
         pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
-        b, c, h, w = src.shape
+        b, c, h, w = src.shape # h = ori_img_h / 16
 
         # Run the transformer
         hs, src = self.transformer(src, pos_src, tokens)
-        iou_token_out = hs[:, 0, :]
+        iou_token_out = hs[:, 0, :]  # hs是tokens用transformer处理后的对应物，所以前几个时对应iou_token与mask_token的，hs.shape=[bs, token_cnt, emb_dim]
         mask_tokens_out = hs[:, 1 : (1 + self.num_mask_tokens), :]
 
         # Upscale mask embeddings and predict masks using the mask tokens
         src = src.transpose(1, 2).view(b, c, h, w)
-        upscaled_embedding = self.output_upscaling(src)
+        upscaled_embedding = self.output_upscaling(src) #转置卷积作4倍上采样，对应文中图14中相应上采样部分
         hyper_in_list: List[torch.Tensor] = []
         for i in range(self.num_mask_tokens):
-            hyper_in_list.append(self.output_hypernetworks_mlps[i](mask_tokens_out[:, i, :]))
-        hyper_in = torch.stack(hyper_in_list, dim=1)
+            hyper_in_list.append(self.output_hypernetworks_mlps[i](mask_tokens_out[:, i, :])) # shape=[bs, emb_dim/8]
+        hyper_in = torch.stack(hyper_in_list, dim=1) # shape = [bs, num_mask_tokens, emb_dim/8]
         b, c, h, w = upscaled_embedding.shape
-        masks = (hyper_in @ upscaled_embedding.view(b, c, h * w)).view(b, -1, h, w)
+        masks = (hyper_in @ upscaled_embedding.view(b, c, h * w)).view(b, -1, h, w) 
+        # mask.shape: [bs, num_mask_token, e/8] @ [bs, e/8, h*w]=> [bs, num_mask_tokens, h*w]=>[bs, num_mask_tokens, h, w]
 
         # Generate mask quality predictions
-        iou_pred = self.iou_prediction_head(iou_token_out)
+        iou_pred = self.iou_prediction_head(iou_token_out) # MLP: => [bs, num_mask_tokens]个Iou预测结果
 
         return masks, iou_pred
 
